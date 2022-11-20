beb8d33a9227a61a1a6cd322f9d3e25359217d97
Xavier Agache <xav.agache@gmail.com>
2022-11-20 16:50:20 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'todo.ionic.angular',
+  appName: 'todo-ionic-angular',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
