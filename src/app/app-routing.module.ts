c8e1c4e8404e34322351a3007dd599cef8e88289
Xavier Agache <xav.agache@gmail.com>
2022-11-20 21:13:24 +0100

    ionic-angular todo app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,8 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
+import { TodosComponent } from './pages/todos/todos.component';
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  { path: '', component: TodosComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { NavComponent } from './pages/nav/nav.component';
+import { TodosComponent } from './pages/todos/todos.component';
+import { TodoItemComponent } from './pages/todo-item/todo-item.component';
+import { FormsModule } from '@angular/forms';
+import { NewTodoComponent } from './pages/new-todo/new-todo.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  declarations: [AppComponent, NavComponent,TodosComponent,TodoItemComponent,NewTodoComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, FormsModule,
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
